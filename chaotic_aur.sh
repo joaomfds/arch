1dece32671b29bd7cbae1ecd468f129d69357831
Joao M. F. Santos <joaomfsantos@hotmail.com>
2024-08-07 10:40:09 +0200

    Update chaotic_aur.sh

--- chaotic_aur.sh
+++ chaotic_aur.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+echo "Adding Chaotic AUR repository"
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+echo " [chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+" >> /etc/pacman.conf "
