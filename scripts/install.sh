2200c4471a0b1c99ac5326153be6f28129762033
Joao Santos <joaomfsantos@hotmail.com>
2025-08-15 06:30:52 +0200

    added stow

--- scripts/install.sh
+++ scripts/install.sh
@@ -58,7 +58,7 @@ sed -i '/ParallelDownloads/c\ParallelDownloads = 50' /etc/pacman.conf
 pacstrap /mnt base linux linux-firmware-intel intel-media-driver intel-gpu-tools \
 intel-ucode base-devel bash-completion aria2 btop duf efibootmgr eza fastfetch \
 fzf fish git man-db man-pages nano ncdu neovide openssh otf-font-awesome \
-reflector starship tailscale yazi zoxide \
+reflector starship stow tailscale yazi zoxide \
 plasma-meta kde-system-meta kde-utilities-meta gnome-boxes \
 hyprland hypridle hyprlock hyprpolkitagent brightnessctl foot grim mako nwg-look \
 pavucontrol qt5ct slurp tlp waybar wl-clip-persist wofi xdg-desktop-portal-hyprland
